92d36f6cde96d4d895e555d48613fc6ef801b8a2
Jonathan Dotore <jonathandotore@outlook.com>
2025-03-27 13:24:04 -0300

    Adding user login dto

---
+++ authJWT/Dto/UserLoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace authJWT.Dto
+{
+    public class UserLoginDto
+    {
+        [Required(ErrorMessage = "Username field is required"), EmailAddress(ErrorMessage = "Invalid e-mail!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password field is required")]
+        public string Password { get; set; }
+    }
+}
